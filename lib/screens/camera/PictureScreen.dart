0db44d1d9151930b818873c663f45aad34cbf75d
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2020-02-27 14:51:51 -0300

    libs for camera

---
+++ lib/screens/camera/PictureScreen.dart
@@ -0,0 +1,83 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+class PictureScreen extends StatefulWidget {
+  final CameraDescription camera; 
+  const PictureScreen({
+    Key key,
+    @required this.camera
+  }) : super(key: key);
+  @override
+  _PictureScreenState createState() => _PictureScreenState();
+}
+
+class _PictureScreenState extends State<PictureScreen> {
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium
+    );
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Camera'),),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Construct the path where the image should be saved using the path
+            // package.
+            final path = join(
+              // Store the picture in the temp directory.
+              // Find the temp directory using the `path_provider` plugin.
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            // Attempt to take a picture and log where it's been saved.
+            await _controller.takePicture(path);
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
