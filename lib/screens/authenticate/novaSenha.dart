cca9c85ce0fbcd6be1097b1af62c9e04ab4eae1d
shpaisman <andre.shpaisman@hotmail.com>
2020-05-17 04:31:06 -0300

    erro solved

--- lib/screens/authenticate/novaSenha.dart
+++ lib/screens/authenticate/novaSenha.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:horta/models/handleErrorLogin.dart';
 import 'package:horta/models/handleErrorRedefinirSenha.dart';
 import 'package:horta/services/auth.dart';
 
@@ -24,7 +23,6 @@ class _NovaSenhaScreenState extends State<NovaSenhaScreen> {
           padding: EdgeInsets.all(66),
           children: <Widget>[
             TextFormField(
-
               onChanged: (val) {
                 setState(() {
                   email = val;
@@ -33,20 +31,33 @@ class _NovaSenhaScreenState extends State<NovaSenhaScreen> {
               decoration: InputDecoration(labelText: 'Email'),
               validator: (val) => val.isEmpty ? 'Insira seu email' : null,
             ),
-             SizedBox(height: 20.0),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0),
-                  ),
+            SizedBox(height: 20.0),
+            Text(
+              error,
+              style: TextStyle(color: Colors.red, fontSize: 14.0),
+            ),
             Container(height: 30),
             RaisedButton(
                 padding: EdgeInsets.all(20),
                 onPressed: () async {
-                  await auth.redefinirSenha(email).then((onValue) {
+                  email == null
+                      ? print("erro")
+                      : await auth.redefinirSenha(email).then((onValue) async {
+                          await showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return CupertinoAlertDialog(
+                                  title: Text(
+                                      "Redefinição de senha bem sucedida."),
+                                  content: Text(
+                                      "Verifique seu e-mail e acesse o link para redefinir sua senha."),
+                                );
+                              });
                           Navigator.pop(context);
                         }).catchError((onError) {
                           setState(() => error =
-                              new HandleErrorRedefinirSenha(onError.code).errorMessage);
+                              new HandleErrorRedefinirSenha(onError.code)
+                                  .errorMessage);
                         });
                 },
                 child: Text("Enviar email de redefinição de senha."))
